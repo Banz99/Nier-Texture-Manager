9d502e68c58df54b7af565b0d56ed684a324789d
Banz99 <42299919+Banz99@users.noreply.github.com>
2018-08-11 17:43:54 +0200

    Add files via upload

---
+++ Decompressor/CompressedFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decompressor
+{
+    class Compressedfile
+    {
+        public bool istexture;
+        public int pointer;
+        public int nameaddress;
+        public int propertyaddress;
+        public int datatype;
+        public int dataoffset;
+        public int datalenght;
+        public int heap;
+        public byte onvolumefile;
+    }
+}
